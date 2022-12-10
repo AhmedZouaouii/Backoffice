0c7abf2c3f519e9b0c550586489f7cf23add5f2f
AhmedZouaouii <110893547+AhmedZouaouii@users.noreply.github.com>
2022-12-10 11:07:46 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,57 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AjouterClubComponent } from './ajouter-club/ajouter-club.component';
+import { AjouterLivreComponent } from './ajouter-livre/ajouter-livre.component';
+
 
 import { AjouterNouveauArticleComponent } from './ajouter-nouveau-article/ajouter-nouveau-article.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ClassePageComponent } from './classroom/classe-page/classe-page.component';
+import { CorbeillePageComponent } from './classroom/corbeille-page/corbeille-page.component';
+import { CourPageComponent } from './classroom/cour-page/cour-page.component';
+import { FluxPageComponent } from './classroom/flux-page/flux-page.component';
+import { HomePageComponent } from './classroom/home-page/home-page.component';
+import { PersonnePageComponent } from './classroom/personne-page/personne-page.component';
+import { UmailPageComponent } from './classroom/umail-page/umail-page.component';
 import { ContentComponent } from './content/content.component';
+import { DetailArticleComponent } from './detail-article/detail-article.component';
+import { InscriptionClubComponent } from './inscription-club/inscription-club.component';
+import { ListeDesArticlesComponent } from './liste-des-articles/liste-des-articles.component';
+import { ListeDesClubsComponent } from './liste-des-clubs/liste-des-clubs.component';
+import { ListesDesLivresComponent } from './liste-des-livres/listes-des-livres.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { TestsComponent } from './tests/tests.component';
+
 
 const routes: Routes = [
  { path: 'element', component: ContentComponent},
- { path: 'article', component: AjouterNouveauArticleComponent}
+ { path: 'article', component: AjouterNouveauArticleComponent},
+ { path:'update/:id', component: AjouterNouveauArticleComponent},
+ { path: 'categorie', component: CategoriesComponent},
+ { path: 'tests', component: TestsComponent},
+ { path: 'listeArticles', component: ListeDesArticlesComponent},
+ { path: 'statistics', component: StatisticsComponent},
+ { path: 'detail', component: DetailArticleComponent},
+ { path: 'classroom/home', component: HomePageComponent},
+ { path: 'classroom/cour', component: CourPageComponent},
+ { path: 'classroom/flux', component: FluxPageComponent},
+ { path: 'classroom/umail', component: UmailPageComponent},
+ { path: 'classroom/classe', component: ClassePageComponent},
+ { path: 'classroom/personne', component: PersonnePageComponent},
+ { path: 'classroom/corbeille', component: CorbeillePageComponent},
+ {path:'ajoutclub',component: AjouterClubComponent},
+{path: 'listeClubs', component: ListeDesClubsComponent},
+{path:'updateClub/:id', component: AjouterClubComponent},
+{path:'inscriptionClub', component:InscriptionClubComponent},
+
+{ path: 'ajoutlivre', component:AjouterLivreComponent},
+{path:'updateLivre/:id', component: AjouterLivreComponent},
+{ path: 'listeLivres', component: ListesDesLivresComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),  FormsModule,],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -8,11 +8,37 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
-import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { ArticleServiceService } from './services/article-service.service';
 import { AjouterNouveauArticleComponent } from './ajouter-nouveau-article/ajouter-nouveau-article.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { TestsComponent } from './tests/tests.component';
+import { ListeDesArticlesComponent } from './liste-des-articles/liste-des-articles.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DetailArticleComponent } from './detail-article/detail-article.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { HomePageComponent } from './classroom/home-page/home-page.component';
+import { CourPageComponent } from './classroom/cour-page/cour-page.component';
+import { FluxPageComponent } from './classroom/flux-page/flux-page.component';
+import { UmailPageComponent } from './classroom/umail-page/umail-page.component';
+import { ClassePageComponent } from './classroom/classe-page/classe-page.component';
+import { PersonnePageComponent } from './classroom/personne-page/personne-page.component';
+import { CorbeillePageComponent } from './classroom/corbeille-page/corbeille-page.component';
+import { CalendarModule, DatePickerModule, DateRangePickerModule, DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+
+import { AjouterClubComponent } from './ajouter-club/ajouter-club.component';
+import { ListeDesClubsComponent } from './liste-des-clubs/liste-des-clubs.component';
+import { DetailClubComponent } from './detail-club/detail-club.component';
+import { InscriptionClubComponent } from './inscription-club/inscription-club.component';
+import { AjouterLivreComponent } from './ajouter-livre/ajouter-livre.component';
+import { ListesDesLivresComponent } from './liste-des-livres/listes-des-livres.component';
+
+
+// Added automatically when the component was created with Angular CLI,
+// but you need to import them manually in StackBlitz.
 
 @NgModule({
   declarations: [
@@ -21,14 +47,48 @@ import { AjouterNouveauArticleComponent } from './ajouter-nouveau-article/ajoute
     SideBarComponent,
     FooterComponent,
     ContentComponent,
-    AjouterNouveauArticleComponent
+    AjouterNouveauArticleComponent,
+    CategoriesComponent,
+    TestsComponent,
+    ListeDesArticlesComponent,
+    DetailArticleComponent,
+    StatisticsComponent,
+    TopBarComponent,
+    SideBarComponent,
+    FooterComponent,
+    ContentComponent,
+    AjouterNouveauArticleComponent,
+    HomePageComponent,
+    CourPageComponent,
+    FluxPageComponent,
+    UmailPageComponent,
+    ClassePageComponent,
+    PersonnePageComponent,
+    CorbeillePageComponent,
+
+    AjouterClubComponent,
+    ListeDesClubsComponent,
+    DetailClubComponent,
+    InscriptionClubComponent,
+  
+    AjouterLivreComponent,
+    ListesDesLivresComponent,
+  
+    
+    
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    CalendarModule, 
+    DatePickerModule, 
+    DateRangePickerModule, 
+    DateTimePickerModule
   ],
   providers: [ArticleServiceService],
   bootstrap: [AppComponent]
